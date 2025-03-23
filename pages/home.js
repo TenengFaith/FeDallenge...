0d07cf2240494d51472401457b49aa29954a5c9d
TenengFaith <.muluhtenengfaithjunior@gmail.com>
2025-03-23 14:57:06 +0100

    home page complete

--- pages/home.js
+++ pages/home.js
@@ -2,8 +2,22 @@
 const getStarted=document.getElementById("getstartedbtn")
 const popup=document.getElementById("popupContainer")
 const closeBtn=document.getElementById("close")
-console.log(getStarted)
+const form=document.getElementById("form-container")
 // events in js are the things that happen when you carry a particular action
 getStarted.addEventListener('click', function(){
     popup.style.display="flex"
+})
+closeBtn.addEventListener('click', function(){
+    popup.style.display="none"
+})
+form.addEventListener('submit', function(){
+    // password and confirm password values
+    const password=document.getElementById("password").value
+    const confirm=document.getElementById("confirm-password").value
+    // value validity
+    if(password!==confirm){
+        alert("password does not match")
+        return;
+    }
+
 })
